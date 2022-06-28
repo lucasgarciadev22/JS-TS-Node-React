a396c0d33abaff57f57f0af2696113911af94a6a
lucasgarciadev22 <lucasgarciaz2018@gmail.com>
2022-06-28 18:51:18 -0300

    Update Pug / CLI

---
+++ testeDesafioCode.js
@@ -0,0 +1,24 @@
+// let months = {
+//   1: "January",
+//   2: "February",
+//   3: "March",
+//   4: "April",
+//   5: "May",
+//   6: "June",
+//   7: "July",
+//   8: "August",
+//   9: "September",
+//   10: "February",
+//   11: "November",
+//   12: "December"
+  
+// };
+
+// let lines = 1;
+
+// //console.log((months.lines).split("\n"));
+// console.log(months[lines]);
+
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados.
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
